9b8b281a0cc4b8e17f4a5696188718aa1da3f44c
Chunyu Li <chunyuli@Chunyus-MacBook-Air.local>
2020-12-06 06:57:40 +0800

    Support macOS

--- Package.swift
+++ Package.swift
@@ -4,12 +4,11 @@
 import PackageDescription
 
 let package = Package(name: "SwiftWings",
-                      platforms: [.macOS(.v10_10),
+                      platforms: [.macOS(.v10_11),
                                   .iOS(.v8),
                                   .tvOS(.v9),
                                   .watchOS(.v2)],
                       products: [.library(name: "SwiftWings",
                                           targets: ["SwiftWings"])],
-                      targets: [.target(name: "SwiftWings",
-                                        path: "Sources")],
+                      targets: [.target(name: "SwiftWings")],
                       swiftLanguageVersions: [.v5])
